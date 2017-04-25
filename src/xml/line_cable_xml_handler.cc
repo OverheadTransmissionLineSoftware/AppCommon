107b3993df283aaca1ead2db0f8bba6da3baad76
bretzel12 <bretzel12@users.noreply.github.com>
2017-04-24 23:19:59 -0700

    Integrated LineCableConnectionXmlHandler into LineCableXmlHandler.
    - Included general documentation cleanup too

--- src/xml/line_cable_xml_handler.cc
+++ src/xml/line_cable_xml_handler.cc
@@ -5,152 +5,7 @@
 
 #include "appcommon/xml/cable_constraint_xml_handler.h"
 #include "appcommon/xml/vector_xml_handler.h"
-
-wxXmlNode* LineCableConnectionXmlHandler::CreateNode(
-    const LineCableConnection& connection,
-    const std::string& name,
-    const std::list<const LineStructure*>* line_structures) {
-  // variables used to create XML node
-  wxXmlNode* node_root = nullptr;
-  wxXmlNode* node_element = nullptr;
-  std::string title;
-  std::string content;
-  wxXmlAttribute attribute;
-
-  // creates a node for the cable constraint root
-  node_root = new wxXmlNode(wxXML_ELEMENT_NODE, "line_cable_connection");
-  node_root->AddAttribute("version", "1");
-
-  if (name != "") {
-    node_root->AddAttribute("name", name);
-  }
-
-  // creates index_structure node and adds to parent node
-  title = "index_structure";
-  if (connection.line_structure != nullptr) {
-    // searches line structure list for a matching line structure
-    auto iter = line_structures->cbegin();
-    while (iter != line_structures->cend()) {
-      const LineStructure* line_structure = *iter;
-      if (line_structure == connection.line_structure) {
-        break;
-      }
-
-      iter++;
-    }
-
-    // gets index, or leaves blank if not found
-    if (iter != line_structures->cend()) {
-      content = std::distance(line_structures->cbegin(), iter);
-    } else {
-      content = *wxEmptyString;
-    }
-  } else {
-    content = *wxEmptyString;
-  }
-  node_element = CreateElementNodeWithContent(title, content);
-  node_root->AddChild(node_element);
-
-  // creates index_attachment node and adds to parent node
-  title = "index_attachment";
-  content = connection.index_attachment;
-  node_element = CreateElementNodeWithContent(title, content);
-  node_root->AddChild(node_element);
-
-  // returns node
-  return node_root;
-}
-
-bool LineCableConnectionXmlHandler::ParseNode(
-    const wxXmlNode* root,
-    const std::string& filepath,
-    const std::list<const LineStructure*>* line_structures,
-    LineCableConnection& connection) {
-  wxString message;
-
-  // checks for valid root node
-  if (root->GetName() != "line_cable_connection") {
-    message = FileAndLineNumber(filepath, root) +
-              " Invalid root node. Aborting node parse.";
-    wxLogError(message);
-    return false;
-  }
-
-  // gets version attribute
-  wxString version;
-  if (root->GetAttribute("version", &version) == false) {
-    message = FileAndLineNumber(filepath, root) +
-              " Version attribute is missing. Aborting node parse.";
-    wxLogError(message);
-    return false;
-  }
-
-  // sends to proper parsing function
-  if (version == "1") {
-    return ParseNodeV1(root, filepath, line_structures, connection);
-  } else {
-    message = FileAndLineNumber(filepath, root) +
-              " Invalid version number. Aborting node parse.";
-    wxLogError(message);
-    return false;
-  }
-}
-
-bool LineCableConnectionXmlHandler::ParseNodeV1(
-    const wxXmlNode* root,
-    const std::string& filepath,
-    const std::list<const LineStructure*>* line_structures,
-    LineCableConnection& connection) {
-  bool status = true;
-  wxString message;
-  long value;
-
-  // evaluates each child node
-  const wxXmlNode* node = root->GetChildren();
-  while (node != nullptr) {
-    const wxString title = node->GetName();
-    const wxString content = ParseElementNodeWithContent(node);
-
-    if (title == "index_structure") {
-      // initializes the line structure reference and assigns to index
-      connection.line_structure = nullptr;
-      long index = content.ToLong(&index);
-
-      // sets pointer from list
-      const int kSize = line_structures->size();
-      if ((0 < index) && (index < kSize)) {
-        connection.line_structure = *std::next(line_structures->cbegin(),
-                                               index);
-      } else {
-        // invalid index
-        message = FileAndLineNumber(filepath, node)
-                  + "Invalid line structure index. Couldn't find " + content;
-        wxLogError(message);
-        status = false;
-      }
-    } else if (title == "index_attachment") {
-      if (content.ToLong(&value) == true) {
-        connection.index_attachment = value;
-      } else {
-        message = FileAndLineNumber(filepath, node)
-                  + "Invalid attachment index.";
-        wxLogError(message);
-        connection.index_attachment = -9999;
-        status = false;
-      }
-    } else {
-      message = FileAndLineNumber(filepath, node)
-                + "XML node isn't recognized.";
-      wxLogError(message);
-      status = false;
-    }
-
-    node = node->GetNext();
-  }
-
-  return status;
-}
-
+#include "models/base/helper.h"
 
 wxXmlNode* LineCableXmlHandler::CreateNode(
     const LineCable& line_cable,
@@ -172,9 +27,7 @@ wxXmlNode* LineCableXmlHandler::CreateNode(
     node_root->AddAttribute("name", name);
   }
 
-  // adds child nodes for struct parameters
-
-  // creates cable node and adds to parent node
+  // creates cable node and adds to root node
   title = "cable";
   if (line_cable.cable() != nullptr) {
     content = line_cable.cable()->name;
@@ -184,12 +37,12 @@ wxXmlNode* LineCableXmlHandler::CreateNode(
   node_element = CreateElementNodeWithContent(title, content);
   node_root->AddChild(node_element);
 
-  // creates constraint node and adds to parent node
+  // creates constraint node and adds to root node
   node_element = CableConstraintXmlHandler::CreateNode(
       line_cable.constraint(), "", units);
   node_root->AddChild(node_element);
 
-  // creates spacing-attachments-ruling-span node and adds to parent node
+  // creates spacing-attachments-ruling-span node and adds to root node
   title = "spacing_attachments_ruling_span";
   if (units == units::UnitSystem::kImperial) {
     attribute = wxXmlAttribute("units", "ft");
@@ -200,7 +53,7 @@ wxXmlNode* LineCableXmlHandler::CreateNode(
       line_cable.spacing_attachments_ruling_span(), title, attribute, 1);
   node_root->AddChild(node_element);
 
-  // creates weathercase-stretch-creep node and adds to parent node
+  // creates weathercase-stretch-creep node and adds to root node
   title = "weather_load_case";
   attribute = wxXmlAttribute("name", "stretch_creep");
   if (line_cable.weathercase_stretch_creep() != nullptr) {
@@ -211,7 +64,7 @@ wxXmlNode* LineCableXmlHandler::CreateNode(
   node_element = CreateElementNodeWithContent(title, content, &attribute);
   node_root->AddChild(node_element);
 
-  // creates weathercase-stretch-creep node and adds to parent node
+  // creates weathercase-stretch-creep node and adds to root node
   title = "weather_load_case";
   attribute = wxXmlAttribute("name", "stretch_load");
   if (line_cable.weathercase_stretch_load() != nullptr) {
@@ -222,22 +75,40 @@ wxXmlNode* LineCableXmlHandler::CreateNode(
   node_element = CreateElementNodeWithContent(title, content, &attribute);
   node_root->AddChild(node_element);
 
-  // creates connections node and adds to parent node
+  // creates connections node and adds to root node
   title = "connections";
   node_element = new wxXmlNode(wxXML_ELEMENT_NODE, title);
+  node_element->AddAttribute("format", "structure_index,attach_index");
 
   for (auto iter = line_cable.connections()->cbegin();
        iter != line_cable.connections()->cend(); iter++) {
     const LineCableConnection& connection = *iter;
-    wxXmlNode* sub_node =
-        LineCableConnectionXmlHandler::CreateNode(connection,
-                                                  "",
-                                                  line_structures);
+
+    // searches line structure list for a matching line structure
+    auto it = line_structures->cbegin();
+    while (it != line_structures->cend()) {
+      const LineStructure* line_structure = *it;
+      if (line_structure == connection.line_structure) {
+        break;
+      }
+
+      it++;
+    }
+
+    // gets structure index
+    const int index_structure = std::distance(line_structures->cbegin(), it);
+    content = std::to_string(index_structure)
+              + "," + std::to_string(connection.index_attachment);
+
+    // creates connection node and adds to connections node
+    wxXmlNode* sub_node = XmlHandler::CreateElementNodeWithContent(
+        "connection",
+        content);
     node_element->AddChild(sub_node);
   }
   node_root->AddChild(node_element);
 
-  // returns node
+  // returns root node
   return node_root;
 }
 
@@ -377,11 +248,47 @@ bool LineCableXmlHandler::ParseNodeV1(
       // gets connection sub-nodes
       wxXmlNode* sub_node = node->GetChildren();
       while (sub_node != nullptr) {
+        const wxString sub_title = sub_node->GetName();
+        const std::string sub_content = ParseElementNodeWithContent(sub_node);
+        long value = -9999;
+
         LineCableConnection connection;
-        const bool status_node = LineCableConnectionXmlHandler::ParseNode(
-            node, filepath, line_structures, connection);
-        if (status_node == false) {
+
+        // parses input string as: structure_index,attach_index
+        std::list<std::string> content_parsed = helper::Parse(sub_content, ',');
+        wxString str;
+
+        // initializes the line structure reference and assigns to index
+        connection.line_structure = nullptr;
+        str = *std::next(content_parsed.cbegin(), 0);
+        if (str.ToLong(&value) == true) {
+          connection.line_structure = *std::next(line_structures->cbegin(), value);
+        } else {
+          message = FileAndLineNumber(filepath, node)
+                    + "Invalid line structure index.";
+          wxLogError(message);
+          connection.line_structure = nullptr;
           status = false;
+          continue;
+        }
+
+        str = *std::next(content_parsed.cbegin(), 1);
+        if (str.ToLong(&value) == true) {
+          connection.index_attachment = value;
+        } else {
+          message = FileAndLineNumber(filepath, node)
+                    + "Invalid attachment index.";
+          wxLogError(message);
+          connection.index_attachment = -9999;
+          status = false;
+        }
+
+        // adds to line cable
+        if (-1 == line_cable.AddConnection(connection)) {
+          status = false;
+          message = FileAndLineNumber(filepath, sub_node)
+                    + "Could not add connection to line cable.";
+          wxLogError(message);
         }
 
         sub_node = sub_node->GetNext();
