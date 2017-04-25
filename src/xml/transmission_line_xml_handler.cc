91a487b60c55de084c4b7c1ed295f2ff5a85150b
bretzel12 <bretzel12@users.noreply.github.com>
2017-04-24 23:25:12 -0700

    Reworked TransmissionLineXmlHandler.
    - Integrated AlignmentXmlHandler
    - Included node name fixes for line_structures and line_cables nodes
    - Added error logging in case LineStructure or LineCable couldn't be
      added to line

--- src/xml/transmission_line_xml_handler.cc
+++ src/xml/transmission_line_xml_handler.cc
@@ -3,7 +3,6 @@
 
 #include "appcommon/xml/transmission_line_xml_handler.h"
 
-#include "appcommon/xml/alignment_xml_handler.h"
 #include "appcommon/xml/line_cable_xml_handler.h"
 #include "appcommon/xml/line_structure_xml_handler.h"
 #include "models/base/helper.h"
@@ -15,9 +14,9 @@ wxXmlNode* TransmissionLineXmlHandler::CreateNode(
   // variables used to create XML node
   wxXmlNode* node_root = nullptr;
   wxXmlNode* node_element = nullptr;
+  wxXmlAttribute attribute;
   std::string title;
   std::string content;
-  wxXmlAttribute attribute;
 
   // creates a node for the root
   node_root = new wxXmlNode(wxXML_ELEMENT_NODE, "transmission_line");
@@ -29,11 +28,36 @@ wxXmlNode* TransmissionLineXmlHandler::CreateNode(
 
   // creates an alignment node and adds to root node
   title = "alignment";
-  node_element = AlignmentXmlHandler::CreateNode(*line.alignment(), "", units);
+  node_element = new wxXmlNode(wxXML_ELEMENT_NODE, title);
+  node_element->AddAttribute("format", "station,elevation,rotation");
+
+  // creates alignment point nodes and adds to alignment node
+  if (units == units::UnitSystem::kMetric) {
+    attribute = wxXmlAttribute("units", "m,m,deg");
+  } else if (units == units::UnitSystem::kImperial) {
+    attribute = wxXmlAttribute("units", "ft,ft,deg");
+  } else {
+    attribute = wxXmlAttribute();
+  }
+
+  const std::list<AlignmentPoint>* points = line.alignment()->points();
+  for (auto iter = points->cbegin(); iter != points->cend();
+       iter++) {
+    const AlignmentPoint& point = *iter;
+    content = helper::DoubleToFormattedString(point.station, 2)
+              + "," + helper::DoubleToFormattedString(point.elevation, 2)
+              + "," + helper::DoubleToFormattedString(point.rotation, 2);
+
+    wxXmlNode* sub_node = XmlHandler::CreateElementNodeWithContent(
+        "point",
+        content,
+        &attribute);
+    node_element->AddChild(sub_node);
+  }
   node_root->AddChild(node_element);
 
   // creates line structures node and adds to root node
-  title = "line structures";
+  title = "line_structures";
   node_element = new wxXmlNode(wxXML_ELEMENT_NODE, title);
 
   const std::list<LineStructure>* line_structures = line.line_structures();
@@ -47,7 +71,7 @@ wxXmlNode* TransmissionLineXmlHandler::CreateNode(
   node_root->AddChild(node_element);
 
   // creates line cables node and adds to root node
-  title = "line cables";
+  title = "line_cables";
   node_element = new wxXmlNode(wxXML_ELEMENT_NODE, title);
 
   std::list<const LineStructure*> const_line_structures;
@@ -67,7 +91,7 @@ wxXmlNode* TransmissionLineXmlHandler::CreateNode(
   }
   node_root->AddChild(node_element);
 
-  // returns node
+  // returns root node
   return node_root;
 }
 
@@ -126,21 +150,77 @@ bool TransmissionLineXmlHandler::ParseNodeV1(
   while (node != nullptr) {
     const wxString title = node->GetName();
     const wxString content = ParseElementNodeWithContent(node);
-    double value = -999999;
 
     if (title == "alignment") {
-      Alignment alignment;
-      const bool status_node = AlignmentXmlHandler::ParseNode(
-          node, filepath, alignment);
-      if (status_node == false) {
-        status = false;
-      }
+      // evaluates each child node
+      const wxXmlNode* sub_node = node->GetChildren();
+      while (sub_node != nullptr) {
+        const wxString sub_title = sub_node->GetName();
+        const std::string sub_content = ParseElementNodeWithContent(sub_node);
+        double value = -999999;
 
-      // adds points to transmission line
-      for (auto iter = alignment.points()->cbegin();
-           iter != alignment.points()->end(); iter++) {
-        const AlignmentPoint& point = *iter;
-        line.AddAlignmentPoint(point);
+        if (sub_title == "point") {
+          AlignmentPoint point;
+
+          // parses input string as: station,elevation,rotation
+          std::list<std::string> content_parsed = helper::Parse(sub_content,
+                                                                ',');
+          wxString str;
+
+          // reads station
+          auto iter = content_parsed.cbegin();
+          str = *iter;
+          if (str.ToDouble(&value) == true) {
+            point.station = value;
+          } else {
+            message = FileAndLineNumber(filepath, sub_node)
+                      + "Invalid station.";
+            wxLogError(message);
+            point.station = -999999;
+            status = false;
+          }
+
+          // reads elevation
+          iter++;
+          str = *iter;
+          if (str.ToDouble(&value) == true) {
+            point.elevation = value;
+          } else {
+            message = FileAndLineNumber(filepath, sub_node)
+                      + "Invalid elevation.";
+            wxLogError(message);
+            point.elevation = -999999;
+            status = false;
+          }
+
+          // reads rotation
+          iter++;
+          str = *iter;
+          if (str.ToDouble(&value) == true) {
+            point.rotation = value;
+          } else {
+            message = FileAndLineNumber(filepath, sub_node)
+                      + "Invalid rotation.";
+            wxLogError(message);
+            point.rotation = -999999;
+            status = false;
+          }
+
+          // adds to transmission line
+          if (-1 == line.AddAlignmentPoint(point)) {
+            status = false;
+            message = FileAndLineNumber(filepath, sub_node)
+                      + "Could not add alignment point to transmission line.";
+            wxLogError(message);
+          }
+        } else {
+          message = FileAndLineNumber(filepath, sub_node)
+                    + "XML node isn't recognized.";
+          wxLogError(message);
+          status = false;
+        }
+
+        sub_node = sub_node->GetNext();
       }
     } else if (title == "line_structures") {
       // gets attachment sub-nodes
@@ -155,7 +235,12 @@ bool TransmissionLineXmlHandler::ParseNodeV1(
         }
 
         // adds to transmission line
-        line.AddLineStructure(line_structure);
+        if (-1 == line.AddLineStructure(line_structure)) {
+          status = false;
+          message = FileAndLineNumber(filepath, sub_node)
+                    + "Could not add line structure to transmission line.";
+          wxLogError(message);
+        }
 
         sub_node = sub_node->GetNext();
       }
@@ -181,7 +266,12 @@ bool TransmissionLineXmlHandler::ParseNodeV1(
         }
 
         // adds to transmission line
-        line.AddLineCable(line_cable);
+        if (-1 == line.AddLineCable(line_cable)) {
+          status = false;
+          message = FileAndLineNumber(filepath, sub_node)
+                    + "Could not add line cable to transmission line.";
+          wxLogError(message);
+        }
 
         sub_node = sub_node->GetNext();
       }
