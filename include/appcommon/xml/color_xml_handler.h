5087c11f5e25ef5ee3e1eed7d648413a2eaa4878
bretzel12 <bretzel12@users.noreply.github.com>
2017-12-10 17:20:55 -0800

    ColorXmlHandler added.

---
+++ include/appcommon/xml/color_xml_handler.h
@@ -0,0 +1,70 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#ifndef OTLS_APPCOMMON_XML_COLORXMLHANDLER_H_
+#define OTLS_APPCOMMON_XML_COLORXMLHANDLER_H_
+
+#include <string>
+#include <list>
+
+#include "wx/xml/xml.h"
+
+#include "appcommon/xml/xml_handler.h"
+
+/// \par OVERVIEW
+///
+/// This class parses and generates a color XML node. The data is transferred
+/// between the XML node and the data object.
+///
+/// \par VERSION
+///
+/// This class can parse all versions of the XML node. However, new nodes will
+/// only be generated with the most recent version.
+class ColorXmlHandler : public XmlHandler {
+ public:
+  /// \brief Creates an XML node for a color.
+  /// \param[in] color
+  ///   The color.
+  /// \param[in] name
+  ///   The name of the XML node. This will be an attribute for the created
+  ///   node. If empty, no attribute will be created.
+  static wxXmlNode* CreateNode(const wxColour& color,
+                               const std::string name);
+
+  /// \brief Parses an XML node and populates a color.
+  /// \param[in] root
+  ///   The XML root node for the color.
+  /// \param[in] filepath
+  ///   The filepath that the xml node was loaded from. This is for logging
+  ///   purposes only and can be left blank.
+  /// \param[out] color
+  ///   The color that is populated.
+  /// \return The status of the xml node parse. If any errors are encountered
+  ///   false is returned.
+  /// All errors are logged to the active application log target. Critical
+  /// errors cause the parsing to abort. Non-critical errors set the object
+  /// property to an invalid state (if applicable).
+  static bool ParseNode(const wxXmlNode* root,
+                        const std::string& filepath,
+                        wxColour& color);
+
+ private:
+  /// \brief Parses a version 1 XML node and populates a color.
+  /// \param[in] root
+  ///   The XML root node for the color.
+  /// \param[in] filepath
+  ///   The filepath that the xml node was loaded from. This is for logging
+  ///   purposes only and can be left blank.
+  /// \param[out] color
+  ///   The color that is populated.
+  /// \return The status of the xml node parse. If any errors are encountered
+  ///   false is returned.
+  /// All errors are logged to the active application log target. Critical
+  /// errors cause the parsing to abort. Non-critical errors set the object
+  /// property to an invalid state (if applicable).
+  static bool ParseNodeV1(const wxXmlNode* root,
+                          const std::string& filepath,
+                          wxColour& color);
+};
+
+#endif  // OTLS_APPCOMMON_XML_COLORXMLHANDLER_H_
---
+++ src/xml/color_xml_handler.cc
@@ -0,0 +1,129 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#include "appcommon/xml/color_xml_handler.h"
+
+#include "models/base/helper.h"
+
+wxXmlNode* ColorXmlHandler::CreateNode(
+    const wxColour& color,
+    const std::string name) {
+  // variables used to create XML node
+  wxXmlNode* node_root = nullptr;
+  std::string title;
+  std::string content;
+
+  // creates a node for the root
+  title = "color";
+  content = std::to_string(color.Red())
+      + "," + std::to_string(color.Green())
+      + "," + std::to_string(color.Blue());
+
+  node_root = CreateElementNodeWithContent(title, content);
+
+  // adds attributes
+  node_root->AddAttribute("version", "1");
+
+  if (name != "") {
+    node_root->AddAttribute("name", name);
+  }
+
+  // returns root node
+  return node_root;
+}
+
+bool ColorXmlHandler::ParseNode(
+    const wxXmlNode* root,
+    const std::string& filepath,
+    wxColour& color) {
+  wxString message;
+
+  // checks for valid root node
+  if (root->GetName() != "color") {
+    message = FileAndLineNumber(filepath, root) +
+              " Invalid root node. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+
+  // gets version attribute
+  wxString version;
+  if (root->GetAttribute("version", &version) == false) {
+    message = FileAndLineNumber(filepath, root) +
+              " Version attribute is missing. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+
+  // sends to proper parsing function
+  if (version == "1") {
+    return ParseNodeV1(root, filepath, color);
+  } else {
+    message = FileAndLineNumber(filepath, root) +
+              " Invalid version number. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+}
+
+bool ColorXmlHandler::ParseNodeV1(
+    const wxXmlNode* root,
+    const std::string& filepath,
+    wxColour& color) {
+  bool status = true;
+  wxString message;
+
+  // parses content into separate strings
+  const wxString content = ParseElementNodeWithContent(root);
+  std::list<std::string> content_parsed = helper::Parse(content, ',');
+  if (content_parsed.size() != 3) {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid color.";
+    wxLogError(message);
+    status = false;
+
+    return false;
+  }
+
+  // converts strings to color component values
+  wxString str;
+  long value = -9999;
+  int r = 0;
+  int g = 0;
+  int b = 0;
+
+  str = *std::next(content_parsed.cbegin(), 0);
+  if (str.ToLong(&value) == true) {
+    r = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid red color component.";
+    wxLogError(message);
+    status = false;
+  }
+
+  str = *std::next(content_parsed.cbegin(), 1);
+  if (str.ToLong(&value) == true) {
+    g = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid green color component.";
+    wxLogError(message);
+    status = false;
+  }
+
+  str = *std::next(content_parsed.cbegin(), 2);
+  if (str.ToLong(&value) == true) {
+    b = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid blue color component.";
+    wxLogError(message);
+    status = false;
+  }
+
+  // modifies color with parsed components
+  color = wxColour(r, g, b);
+
+  return status;
+}
