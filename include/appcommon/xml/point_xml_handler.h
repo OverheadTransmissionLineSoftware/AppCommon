83a4845cfec3300a008ecbb2dc94cfaec3affdf0
bretzel12 <bretzel12@users.noreply.github.com>
2018-02-20 18:52:20 -0800

    Point2dXmlHandler and Point3dXmlHandler added.

---
+++ include/appcommon/xml/point_xml_handler.h
@@ -0,0 +1,141 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#ifndef OTLS_APPCOMMON_XML_POINTXMLHANDLER_H_
+#define OTLS_APPCOMMON_XML_POINTXMLHANDLER_H_
+
+#include <string>
+
+#include "models/base/point.h"
+#include "wx/xml/xml.h"
+
+#include "appcommon/xml/xml_handler.h"
+
+/// \par OVERVIEW
+///
+/// This class parses and generates a Point2d<double> XML node. The data is
+/// transferred between the XML node and the data object.
+///
+/// \par VERSION
+///
+/// This class can parse all versions of the XML node. However, new nodes will
+/// only be generated with the most recent version.
+class Point2dDoubleXmlHandler : public XmlHandler {
+ public:
+  /// \brief Creates an XML node for the point.
+  /// \param[in] point
+  ///   The point.
+  /// \param[in] name
+  ///   The name of the XML node. This will be an attribute for the created
+  ///   node. If empty, no attribute will be created.
+  /// \param[in] attribute_components
+  ///   The attribute that will be created for the node.
+  /// \param[in] precision
+  ///   The rounding precision for the components.
+  /// \return An XML node for the point.
+  static wxXmlNode* CreateNode(const Point2d<double>& point,
+                               const std::string& name,
+                               const wxXmlAttribute& attribute_components,
+                               const int& precision);
+
+  /// \brief Parses an XML node and populates a point.
+  /// \param[in] root
+  ///   The XML root node for the point.
+  /// \param[in] filepath
+  ///   The filepath that the xml node was loaded from. This is for logging
+  ///   purposes only and can be left blank.
+  /// \param[out] point
+  ///   The point that is populated.
+  /// \return The status of the xml node parse. If any errors are encountered
+  ///   false is returned.
+  /// All errors are logged to the active application log target. Critical
+  /// errors cause the parsing to abort. Non-critical errors set the object
+  /// property to an invalid state (if applicable).
+  static bool ParseNode(const wxXmlNode* root,
+                        const std::string& filepath,
+                        Point2d<double>& point);
+
+ private:
+  /// \brief Parses a version 1 XML node and populates a point.
+  /// \param[in] root
+  ///   The XML root node for the point.
+  /// \param[in] filepath
+  ///   The filepath that the xml node was loaded from. This is for logging
+  ///   purposes only and can be left blank.
+  /// \param[out] point
+  ///   The point that is populated.
+  /// \return The status of the xml node parse. If any errors are encountered
+  ///   false is returned.
+  /// All errors are logged to the active application log target. Critical
+  /// errors cause the parsing to abort. Non-critical errors set the object
+  /// property to an invalid state (if applicable).
+  static bool ParseNodeV1(const wxXmlNode* root,
+                          const std::string& filepath,
+                          Point2d<double>& point);
+};
+
+
+/// \par OVERVIEW
+///
+/// This class parses and generates a Point3d<double> XML node. The data is
+/// transferred between the XML node and the data object.
+///
+/// \par VERSION
+///
+/// This class can parse all versions of the XML node. However, new nodes will
+/// only be generated with the most recent version.
+class Point3dDoubleXmlHandler : public XmlHandler {
+ public:
+  /// \brief Creates an XML node for the point.
+  /// \param[in] point
+  ///   The point.
+  /// \param[in] name
+  ///   The name of the XML node. This will be an attribute for the created
+  ///   node. If empty, no attribute will be created.
+  /// \param[in] attribute_components
+  ///   The attribute that will be created for the node.
+  /// \param[in] precision
+  ///   The rounding precision for the components.
+  /// \return An XML node for the point.
+  static wxXmlNode* CreateNode(const Point3d<double>& point,
+                               const std::string& name,
+                               const wxXmlAttribute& attribute_components,
+                               const int& precision);
+
+  /// \brief Parses an XML node and populates a point.
+  /// \param[in] root
+  ///   The XML root node for the point.
+  /// \param[in] filepath
+  ///   The filepath that the xml node was loaded from. This is for logging
+  ///   purposes only and can be left blank.
+  /// \param[out] point
+  ///   The point that is populated.
+  /// \return The status of the xml node parse. If any errors are encountered
+  ///   false is returned.
+  /// All errors are logged to the active application log target. Critical
+  /// errors cause the parsing to abort. Non-critical errors set the object
+  /// property to an invalid state (if applicable).
+  static bool ParseNode(const wxXmlNode* root,
+                        const std::string& filepath,
+                        Point3d<double>& point);
+
+ private:
+  /// \brief Parses a version 1 XML node and populates a point.
+  /// \param[in] root
+  ///   The XML root node for the point.
+  /// \param[in] filepath
+  ///   The filepath that the xml node was loaded from. This is for logging
+  ///   purposes only and can be left blank.
+  /// \param[out] point
+  ///   The point that is populated.
+  /// \return The status of the xml node parse. If any errors are encountered
+  ///   false is returned.
+  /// All errors are logged to the active application log target. Critical
+  /// errors cause the parsing to abort. Non-critical errors set the object
+  /// property to an invalid state (if applicable).
+  static bool ParseNodeV1(const wxXmlNode* root,
+                          const std::string& filepath,
+                          Point3d<double>& point);
+};
+
+#endif  // OTLS_APPCOMMON_XML_POINTXMLHANDLER_H_
---
+++ src/xml/point_xml_handler.cc
@@ -0,0 +1,230 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#include "appcommon/xml/point_xml_handler.h"
+
+#include "models/base/helper.h"
+
+wxXmlNode* Point2dDoubleXmlHandler::CreateNode(
+    const Point2d<double>& point,
+    const std::string& name,
+    const wxXmlAttribute& attribute_components,
+    const int& precision) {
+  // variables used to create XML node
+  wxXmlNode* node_root = nullptr;
+  std::string title;
+  std::string content;
+
+  // creates a node for the root
+  title = "point_2d";
+  content = helper::DoubleToFormattedString(point.x, precision)
+      + "," + helper::DoubleToFormattedString(point.y, precision);
+
+  node_root = CreateElementNodeWithContent(title, content);
+
+  // adds attributes
+  node_root->AddAttribute("version", "1");
+
+  if (name != "") {
+    node_root->AddAttribute("name", name);
+  }
+
+  // returns node
+  return node_root;
+}
+
+bool Point2dDoubleXmlHandler::ParseNode(const wxXmlNode* root,
+                                        const std::string& filepath,
+                                        Point2d<double>& point) {
+  wxString message;
+
+  // checks for valid root node
+  if (root->GetName() != "point_2d") {
+    message = FileAndLineNumber(filepath, root) +
+              " Invalid root node. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+
+  // gets version attribute
+  wxString version;
+  if (root->GetAttribute("version", &version) == false) {
+    message = FileAndLineNumber(filepath, root) +
+              " Version attribute is missing. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+
+  // sends to proper parsing function
+  if (version == "1") {
+    return ParseNodeV1(root, filepath, point);
+  } else {
+    message = FileAndLineNumber(filepath, root) +
+              " Invalid version number. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+}
+
+bool Point2dDoubleXmlHandler::ParseNodeV1(const wxXmlNode* root,
+                                          const std::string& filepath,
+                                          Point2d<double>& point) {
+  bool status = true;
+  wxString message;
+
+  // parses content into separate strings
+  const std::string content = ParseElementNodeWithContent(root);
+  std::list<std::string> content_parsed = helper::Parse(content, ',');
+  if (content_parsed.size() != 2) {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid point.";
+    wxLogError(message);
+    status = false;
+
+    return false;
+  }
+
+  // converts strings to double values
+  wxString str;
+  double value = -999999;
+
+  str = *std::next(content_parsed.cbegin(), 0);
+  if (str.ToDouble(&value) == true) {
+    point.x = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid x value.";
+    wxLogError(message);
+    status = false;
+  }
+
+  str = *std::next(content_parsed.cbegin(), 1);
+  if (str.ToDouble(&value) == true) {
+    point.y = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid y value.";
+    wxLogError(message);
+    status = false;
+  }
+
+  return status;
+}
+
+
+wxXmlNode* Point3dDoubleXmlHandler::CreateNode(
+    const Point3d<double>& point,
+    const std::string& name,
+    const wxXmlAttribute& attribute_components,
+    const int& precision) {
+  // variables used to create XML node
+  wxXmlNode* node_root = nullptr;
+  std::string title;
+  std::string content;
+
+  // creates a node for the root
+  title = "point_3d";
+  content = helper::DoubleToFormattedString(point.x, precision)
+      + "," + helper::DoubleToFormattedString(point.y, precision)
+      + "," + helper::DoubleToFormattedString(point.z, precision);
+
+  node_root = CreateElementNodeWithContent(title, content);
+
+  // adds attributes
+  node_root->AddAttribute("version", "1");
+
+  if (name != "") {
+    node_root->AddAttribute("name", name);
+  }
+
+  // returns node
+  return node_root;
+}
+
+bool Point3dDoubleXmlHandler::ParseNode(const wxXmlNode* root,
+                                        const std::string& filepath,
+                                        Point3d<double>& point) {
+  wxString message;
+
+  // checks for valid root node
+  if (root->GetName() != "point_3d") {
+    message = FileAndLineNumber(filepath, root) +
+              " Invalid root node. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+
+  // gets version attribute
+  wxString version;
+  if (root->GetAttribute("version", &version) == false) {
+    message = FileAndLineNumber(filepath, root) +
+              " Version attribute is missing. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+
+  // sends to proper parsing function
+  if (version == "1") {
+    return ParseNodeV1(root, filepath, point);
+  } else {
+    message = FileAndLineNumber(filepath, root) +
+              " Invalid version number. Aborting node parse.";
+    wxLogError(message);
+    return false;
+  }
+}
+
+bool Point3dDoubleXmlHandler::ParseNodeV1(const wxXmlNode* root,
+                                          const std::string& filepath,
+                                          Point3d<double>& point) {
+  bool status = true;
+  wxString message;
+
+  // parses content into separate strings
+  const std::string content = ParseElementNodeWithContent(root);
+  std::list<std::string> content_parsed = helper::Parse(content, ',');
+  if (content_parsed.size() != 3) {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid point.";
+    wxLogError(message);
+    status = false;
+
+    return false;
+  }
+
+  // converts strings to double values
+  wxString str;
+  double value = -999999;
+
+  str = *std::next(content_parsed.cbegin(), 0);
+  if (str.ToDouble(&value) == true) {
+    point.x = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid x value.";
+    wxLogError(message);
+    status = false;
+  }
+
+  str = *std::next(content_parsed.cbegin(), 1);
+  if (str.ToDouble(&value) == true) {
+    point.y = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid y value.";
+    wxLogError(message);
+    status = false;
+  }
+
+  str = *std::next(content_parsed.cbegin(), 1);
+  if (str.ToDouble(&value) == true) {
+    point.z = value;
+  } else {
+    message = FileAndLineNumber(filepath, root)
+              + "Invalid z value.";
+    wxLogError(message);
+    status = false;
+  }
+
+  return status;
+}
